1a81d3285a6fab25cd08eb28f0dfcda91429b094
zhenya <leonov@gmail.com>
2020-02-28 11:00:32 -0800

    Package description

--- src/main/java/software/leonov/quantify/package-info.java
+++ src/main/java/software/leonov/quantify/package-info.java
@@ -1,4 +1,4 @@
 /**
- * Enum utilities to represent and convert digital quantities of bits and bytes.
+ * Utilities to represent and convert digital quantities of bits and bytes.
  */
 package software.leonov.quantify;
